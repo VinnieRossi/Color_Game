fcd1d0ef82ca1a651b513091d4ffd5c1af8092ac
Vinnie <vinnie.rossi12@gmail.com>
2016-03-25 06:23:16 -0400

    Fixed screen issue for now. Started refactoring codebase

--- app/src/main/java/com/vinal/color_game/Game.java
+++ app/src/main/java/com/vinal/color_game/Game.java
@@ -2,21 +2,19 @@ package com.vinal.color_game;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
-import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -33,7 +31,6 @@ import java.util.Random;
  */
 public class Game extends Activity implements View.OnTouchListener{
 
-    private Handler aHandler = new Handler();
     private Thread thread;
 
     private ImageView playScreen;
@@ -52,14 +49,15 @@ public class Game extends Activity implements View.OnTouchListener{
     private FileInputStream mp3Stream;
 
     private List<Rect> rect;
-    private List<Integer> colors;
+    private ArrayList<Integer> colors = new ArrayList<>();
     private String currentSettings;
     private int difficulty = 1;
     private int prevScore;
     private int life = 3;
-    private boolean shouldRun = true;
+    private volatile boolean shouldRun = true;
 
     protected void onCreate(Bundle savedInstanceState) {
+    // **Have a game state object with important variables**
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_layout);
@@ -71,29 +69,24 @@ public class Game extends Activity implements View.OnTouchListener{
         x3 = (ImageView) findViewById(R.id.x3);
         check1 = (ImageView) findViewById(R.id.check1);
         check2 = (ImageView) findViewById(R.id.check2);
+        // fix unnecessary image views
 
-
+        //create setup function?
         SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
         prevScore = prefs.getInt("key", 0);
         currentSettings = prefs.getString("settings", "011");
 
         canvas = new Canvas();
         paint = new Paint();
-        //Bitmap bitmap = Bitmap.createBitmap(1080, 1600, Bitmap.Config.ARGB_8888);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        Bitmap bitmap = Bitmap.createBitmap(metrics.widthPixels, metrics.heightPixels - 425, Bitmap.Config.ARGB_8888);
 
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        Log.e("DIMENSIONS", "" + size.x + ", " + size.y);
-        // make it proper size of screen, then fit it to size given?
-        // right now, can crash if clicking NOT in canvas
-        // it checks x,y and finds it isnt in area
-
-        Bitmap bitmap = Bitmap.createBitmap(1415, 2115, Bitmap.Config.ARGB_8888);
         canvas.setBitmap(bitmap);
         playScreen.setImageBitmap(bitmap);
-        playScreen.setBackgroundColor(getResources().getColor(R.color.black));
+        playScreen.setBackgroundColor(ContextCompat.getColor(this, R.color.black));
         playScreen.setOnTouchListener(this);
+
 
         String mp3File = "raw/popping.mp3";
         assetMan = getAssets();
@@ -106,64 +99,62 @@ public class Game extends Activity implements View.OnTouchListener{
             e1.printStackTrace();
         }
 
-        rect = new ArrayList<Rect>();
+        rect = new ArrayList<>();
 
-        // Just for testing with circles
-        final ArrayList<Integer> colors = new ArrayList<Integer>();
-        colors.add(getResources().getColor(R.color.red));
-        colors.add(getResources().getColor(R.color.red_orange));
-        colors.add(getResources().getColor(R.color.orange));
-        colors.add(getResources().getColor(R.color.yellow));
-        colors.add(getResources().getColor(R.color.green));
-        colors.add(getResources().getColor(R.color.turquoise)); //5
-        colors.add(getResources().getColor(R.color.blue));
-        colors.add(getResources().getColor(R.color.violet_blue));
-        colors.add(getResources().getColor(R.color.violet));
-        colors.add(getResources().getColor(R.color.indigo));
-        colors.add(getResources().getColor(R.color.white)); //10
-        colors.add(getResources().getColor(R.color.gray_cloud));
-        colors.add(getResources().getColor(R.color.gray));
-        colors.add(getResources().getColor(R.color.gray_dolphin));
-        colors.add(getResources().getColor(R.color.black_cat));
+        // create method that initialize list, easier testing
+        colors.add(ContextCompat.getColor(this, R.color.red));
+        colors.add(ContextCompat.getColor(this, R.color.red_orange));
+        colors.add(ContextCompat.getColor(this, R.color.orange));
+        colors.add(ContextCompat.getColor(this, R.color.yellow));
+        colors.add(ContextCompat.getColor(this, R.color.green));
+        colors.add(ContextCompat.getColor(this, R.color.turquoise));
+        colors.add(ContextCompat.getColor(this, R.color.blue));
+        colors.add(ContextCompat.getColor(this, R.color.violet_blue));
+        colors.add(ContextCompat.getColor(this, R.color.violet));
+        colors.add(ContextCompat.getColor(this, R.color.indigo));
+        colors.add(ContextCompat.getColor(this, R.color.white));
+        colors.add(ContextCompat.getColor(this, R.color.gray_cloud));
+        colors.add(ContextCompat.getColor(this, R.color.gray));
+        colors.add(ContextCompat.getColor(this, R.color.gray_dolphin));
+        colors.add(ContextCompat.getColor(this, R.color.black_cat));
 
+        // startGame()
         thread = new Thread() {
             public void run() {
+                int x, y, rad, color, numTries;
                 while (shouldRun) {
                     try {
                         Thread.sleep(1500 / difficulty);
-                        int x = rn.nextInt(playScreen.getWidth());
-                        int y = rn.nextInt(playScreen.getHeight());
-                        int rad = rn.nextInt(100) + (45 - (5 * difficulty));//100, 25, 3
-                        int color = colors.get(rn.nextInt(colors.size()-5));
-                        int numTries = 0;
-                        boolean checkOne = false;
-                        boolean checkTwo = false;
-                        boolean checkThree = false;
+                        x = rn.nextInt(playScreen.getWidth());
+                        y = rn.nextInt(playScreen.getHeight());
+                        rad = rn.nextInt(100) + (45 - (5 * difficulty));//100, 25, 3
+                        color = colors.get(rn.nextInt(colors.size()-5));
+                        numTries = 0;
 
-                        while (!(checkOne && checkTwo && checkThree)) {
+                        while (xNotWithinBounds(x) || yNotWithinBounds(y) || haveOverlappingCircle(x, y, rad)) {
                             x = rn.nextInt(playScreen.getWidth());
                             y = rn.nextInt(playScreen.getHeight());
 
-                            checkOne = !(x < 120 || x > 960);
-                            checkTwo = !(y < 120 || y > 1480);
-                            checkThree = !(isInRectangle(x, y, rad));
-
+                            // make decision based on number of tries
+                             checkGameState(numTries, rad);
+/*
                             if (numTries > 35) {
                                 if (rad > 20) {
                                     rad--;
                                 } else {
-                                    //start warning player
-                                    System.out.println("Game about to end");
-                                    if (numTries > 75) {
-                                        //color
-                                    } else if (numTries > 100) {
+                                    if (numTries > 100) {
                                         onPause();
+                                    } else if (numTries > 75) {
+                                        // warn user about ending game
+                                        Log.e("SYSTEM INFO", "GAME ABOUT TO END");
                                     }
                                 }
                             }
                             numTries++;
+                            */
                         }
 
+                        // drawOutlineRectangle()
                         paint.setStyle(Paint.Style.STROKE);
                         paint.setColor(getResources().getColor(R.color.black));
                         //paint.setColor(getResources().getColor(R.color.white)); // For testing
@@ -172,6 +163,7 @@ public class Game extends Activity implements View.OnTouchListener{
                         canvas.drawRect(temp, paint);
                         rect.add(temp);
 
+                        // setThemeColor()
                         paint.setStyle(Paint.Style.FILL);
                         if (getCurrentTheme() == 0) {
                             paint.setColor(color);
@@ -184,29 +176,14 @@ public class Game extends Activity implements View.OnTouchListener{
                         }
 
                         canvas.drawCircle(x, y, rad, paint);
-                        // For image instead
-                        //Bitmap bt = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.color_wheel), 2*rad, 2*rad, false);
-                        //canvas.drawBitmap(bt, x-rad, y-rad, paint);
 
-                        // For target/sweet spot
-                        /*
-                                paint.setColor(getResources().getColor(R.color.white));//target depend on circle color?
-                                canvas.drawCircle(x, y, rad/4, paint);
-                                paint.setColor(getResources().getColor(R.color.red));
-                                canvas.drawCircle(x, y, rad/6, paint);
-                                paint.setColor(getResources().getColor(R.color.white));
-                                canvas.drawCircle(x, y, rad/8, paint);
-                                paint.setColor(getResources().getColor(R.color.red));
-                                canvas.drawCircle(x, y, rad/16, paint);
-
-                        */
                         runOnUiThread(new Runnable() {
                             public void run() {
                                 playScreen.invalidate();
                             }
                         });
 
-                    } catch (Exception e) {
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
@@ -254,26 +231,13 @@ public class Game extends Activity implements View.OnTouchListener{
         return true;
     }
 
-    public boolean isInRectangle(int x, int y, int radius) {
-        for (int i = 0; i < rect.size(); i++) {
-            if ((x > rect.get(i).left - radius && x < rect.get(i).right + radius) && (y > rect.get(i).bottom - radius && y < rect.get(i).top + radius)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
-    public boolean isInCircle(int x, int y) {
+    private boolean isInCircle(int x, int y) {
         Bitmap bitmap = ((BitmapDrawable) playScreen.getDrawable()).getBitmap();
-        Log.e("X", "" + x);
-        Log.e("Y", "" + y);
-        Log.e("COLOR", "" + bitmap.getPixel(x, y));
-        return bitmap.getPixel(x, y) != 0 && bitmap.getPixel(x, y) != getResources().getColor(R.color.black);
-        //return bitmap.getPixel(x, y) != 0 && bitmap.getPixel(x, y) != -16777216; // Check if it's black
-
+        return bitmap.getPixel(x, y) != 0 && bitmap.getPixel(x, y) != ContextCompat.getColor(this, R.color.black);
     }
 
-    public Rect getContainingRect(int x, int y) {
+    private Rect getContainingRect(int x, int y) {
 
         for (int i = 0; i < rect.size(); i++) {
             // contains doesn't seem to work..looks like y value is messed up
@@ -284,7 +248,7 @@ public class Game extends Activity implements View.OnTouchListener{
         return new Rect();
     }
 
-    public int getContainingRectIndex(Rect toFind) {
+    private int getContainingRectIndex(Rect toFind) {
         for (int i = 0; i < rect.size(); i++) {
             if ((toFind.left == rect.get(i).left && toFind.right == rect.get(i).right) && (toFind.bottom == rect.get(i).bottom && toFind.top == rect.get(i).top)) {
                 return i;
@@ -293,7 +257,7 @@ public class Game extends Activity implements View.OnTouchListener{
         return -1;
     }
 
-    public void removeCircle(int x, int y) {
+    private void removeCircle(int x, int y) {
 
         Rect toRemove = getContainingRect(x, y);
         paint.setColor(getResources().getColor(R.color.black));
@@ -302,11 +266,11 @@ public class Game extends Activity implements View.OnTouchListener{
         playScreen.invalidate();
     }
 
-    public int getRadius(Rect rect) {
+    private int getRadius(Rect rect) {
         return (rect.right - rect.left)/2;
     }
 
-    public void addPoints(int points) {
+    private void addPoints(int points) {
 
         String pointScore = (String) score.getText();
         int scoreAsInt = Integer.parseInt(pointScore) + (difficulty*(120-points));
@@ -332,7 +296,7 @@ public class Game extends Activity implements View.OnTouchListener{
         }
     }
 
-    public void removeLife() {
+    private void removeLife() {
 
         life -= 1; //change color of background?
 
@@ -358,7 +322,7 @@ public class Game extends Activity implements View.OnTouchListener{
         }
     }
 
-    public void addLife() {
+    private void addLife() {
         life += 1;
         switch (life) {
             case 1:
@@ -380,7 +344,6 @@ public class Game extends Activity implements View.OnTouchListener{
         }
     }
 
-
     protected void onPause() {
         super.onPause();
 
@@ -398,20 +361,70 @@ public class Game extends Activity implements View.OnTouchListener{
         finish();
     }
 
-    public void lostSequence() {
+    private void lostSequence() {
         //use invisible fragment, turn visible and prompt/turn on buttons/etc
         playScreen.setBackgroundColor(getResources().getColor(R.color.black)); //ask to play again
     }
 
-    public boolean getSoundOn() {
+    private boolean getSoundOn() {
         return currentSettings.substring(1, 2).equals("1");
     }
 
-    public boolean getPowerUpOn() {
+    private boolean getPowerUpOn() {
         return currentSettings.substring(2).equals("1");
     }
 
-    public int getCurrentTheme() {
+    private int getCurrentTheme() {
         return Integer.parseInt(currentSettings.substring(0, 1));
     }
+
+    private boolean xNotWithinBounds(int x) {
+        return x < 120 || x > 1320;
+    }
+
+    private boolean yNotWithinBounds(int y) {
+        return y < 120 || y > 2015;
+    }
+
+    private boolean haveOverlappingCircle(int x, int y, int radius) {
+        for (int i = 0; i < rect.size(); i++) {
+            if ((x > rect.get(i).left - radius && x < rect.get(i).right + radius) && (y > rect.get(i).bottom - radius && y < rect.get(i).top + radius)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkGameState(int numTries, int rad) {
+        if (numTries > 35) {
+            if (rad > 20) {
+                rad--;
+            } else {
+                if (numTries > 100) {
+                    onPause();
+                } else if (numTries > 75) {
+                    // warn user about ending game
+                    Log.e("SYSTEM INFO", "GAME ABOUT TO END");
+                }
+            }
+        }
+        numTries++;
+    }
 }
+
+/*
+// For image instead
+Bitmap bt = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.color_wheel), 2*rad, 2*rad, false);
+canvas.drawBitmap(bt, x-rad, y-rad, paint);
+
+// For target/sweet spot
+paint.setColor(getResources().getColor(R.color.white));//target depend on circle color?
+canvas.drawCircle(x, y, rad/4, paint);
+paint.setColor(getResources().getColor(R.color.red));
+canvas.drawCircle(x, y, rad/6, paint);
+paint.setColor(getResources().getColor(R.color.white));
+canvas.drawCircle(x, y, rad/8, paint);
+paint.setColor(getResources().getColor(R.color.red));
+canvas.drawCircle(x, y, rad/16, paint);
+
+*/
